eb2f8cacf17c4925453c3eb1a8dfd2094f7d25c9
Christophe <harris.christophe@gmail.com>
2016-04-11 23:37:46 +0100

    not getting error now in the while loops

--- app/src/main/java/com/example/chris/cursorloop/MainActivity.java
+++ app/src/main/java/com/example/chris/cursorloop/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
 
     Cursor phones;
     Cursor phonestwo;
+    Cursor people;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,50 +36,86 @@ public class MainActivity extends Activity {
                 null,
 //                display in ascending order
                 ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
+        Log.e("phones", "" + phones.getCount());
 
-
-Log.e("phones", "" + phones.getCount());
-
-
+//        phones.close();
+//
+//
+        phones.moveToFirst();
         String phoneContactId = phones.getString(phones.getColumnIndexOrThrow(BaseColumns._ID));
+        Log.e("phonecontactid", "" + phoneContactId);
+        phones.close();
+
 //        if (phonestwo.isBeforeFirst()) {
 //        phonestwo.moveToFirst();}
 //        while (phonestwo.moveToNext()) {
 //        if(phonestwo != null &&  phonestwo.moveToFirst()){
 //        phonestwo.moveToFirst();
         if(phonestwo!=null && phonestwo.getCount()>0){
-            phonestwo.moveToFirst();
-            phonestwo = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    null,
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                    new String[]{phoneContactId},
-                    null);
-            phonestwo.close();
-        }
-
+        phonestwo.moveToFirst();}
+        phonestwo = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                new String[]{phoneContactId},
+                null);
         Log.e("phonestwo", "" + phonestwo.getCount());
+        phonestwo.close();
+
 
     }
+//
+//
+//        Log.e("phonestwo", "" + phonestwo.getCount());
+//
+//    }
+//*****************************
+//         people = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+//        if (people != null && people.getCount() > 0) {
+//            people.moveToFirst();
+//        }
+//        while(people.moveToNext()) {
+//
+//
+//
+//                int nameFieldColumnIndex = people.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME);
+//                String contact = people.getString(nameFieldColumnIndex);
+//                int numberFieldColumnIndex = people.getColumnIndex(ContactsContract.PhoneLookup.NUMBER);
+//                String number = people.getString(numberFieldColumnIndex);
+//            }
+//
+//        Log.e("people", "" + people.getCount());
+//        people.close();
+//        *******************************************
+//         phonestwo = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+//
+//        while (phonestwo.moveToNext())
+//        {
+//            String NameofPerson=phonestwo.getString(phonestwo.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+//            String NumberofPerson=phonestwo.getString(phonestwo.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//        }
+//        Log.e("Name", "" + ());
+//    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
